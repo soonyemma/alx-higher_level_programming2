713633c01780ea9cb13152a2d5f31e41d94ca958
soonyemma <69128107+soonyemma@users.noreply.github.com>
2022-06-28 13:22:43 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Martins Akhivbareme
+# Author - Somtochukwu Onyemma
 
 """Print the numbers from 1 to 100 separated by a space.
   For multiples of three, print Fizz instead of the number
